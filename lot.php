e10521e1221bacdd829070872603ba5541e2d3d3
Suren <30807738+shadkeeper@users.noreply.github.com>
2017-11-28 11:17:00 +0300

    делаем задание 4-1 вроде сделал

--- lot.php
+++ lot.php
@@ -1,49 +1,37 @@
 <?php
-$is_auth = (bool)rand(0, 1);
 
-$user_name = 'Константин';
-$user_avatar = 'img/user.jpg';
-
-// ставки пользователей, которыми надо заполнить таблицу
-$bets = [
-    ['name' => 'Иван', 'price' => 11500, 'ts' => strtotime('-' . rand(1, 50) .' minute')],
-    ['name' => 'Константин', 'price' => 11000, 'ts' => strtotime('-' . rand(1, 18) .' hour')],
-    ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
-    ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
-];
-
-date_default_timezone_set('Europe/Moscow');
-function timeFun($timefun) {
-    $now = strtotime('now');
-    $fun = ($now - $timefun) / 60;
-    if ($fun / 60 < 24) {
-        if ($fun < 60) {
-            $tsr = ceil($fun) . ' минут назад';
-        } else {
-            $tsr = ceil($fun / 60) . ' часов назад';
-        }
-    }
-    else {
-           $tsr = date("d:m:y в H:i", $timefun);
-        }
-    return $tsr;
-};
-
+require ('data.php');
 require ('functions.php');
 
-$page_content = render_page('lot',['bets' => $bets]);
+$lot = null;
+if (isset($_GET['id'])) {
+	$lot_id = $_GET['id'];
+	foreach ($goods as $item) {
+		if ($item['id'] == $lot_id) {
+			$lot = $item;
+			break;
+		}
+	}
+}
+if (!$lot) {
+	http_response_code(404);
+}
 
+$page_content = render_page('lot',
+	[
+		'lot' => $lot,
+		'bets' => $bets
+	]);
 $layout_content = render_page('layout',
-    [
-        'is_auth' => $is_auth,
-        'user_name' => $user_name,
-        'user_avatar' => $user_avatar,
-        'content' => $page_content,
-        'title' => 'DC Ply Mens 2016/2017 Snowboard'
-    ]
-);
+	[
+    	'is_auth' => $is_auth,
+    	'user_name' => $user_name,
+    	'user_avatar' => $user_avatar,
+		'content' => $page_content,
+		'title' => 'Лот №' . $lot['id'] . ' - ' . $lot['name']
+	]);
 
-print ($layout_content);
+print($layout_content);
 
 ?>
 
--- templates/lot.php
+++ templates/lot.php
@@ -1,21 +1,13 @@
 <section class="lot-item container">
-    <h2>DC Ply Mens 2016/2017 Snowboard</h2>
+    <?php if (isset($lot)): ?>
+    <h2><?=htmlspecialchars($lot['name']);?></h2>
     <div class="lot-item__content">
         <div class="lot-item__left">
             <div class="lot-item__image">
-                <img src="img/lot-image.jpg" width="730" height="548" alt="Сноуборд">
+                <img src="<?=$lot['url_img']; ?>" width="730" height="548" alt="<?=$lot['img_alt']; ?>">
             </div>
-            <p class="lot-item__category">Категория: <span>Доски и лыжи</span></p>
-            <p class="lot-item__description">Легкий маневренный сноуборд, готовый дать жару в любом парке, растопив
-                снег
-                мощным щелчкоми четкими дугами. Стекловолокно Bi-Ax, уложенное в двух направлениях, наделяет этот
-                снаряд
-                отличной гибкостью и отзывчивостью, а симметричная геометрия в сочетании с классическим прогибом
-                кэмбер
-                позволит уверенно держать высокие скорости. А если к концу катального дня сил совсем не останется,
-                просто
-                посмотрите на Вашу доску и улыбнитесь, крутая графика от Шона Кливера еще никого не оставляла
-                равнодушным.</p>
+            <p class="lot-item__category">Категория: <span><?=$lot['category']; ?></span></p>
+            <p class="lot-item__description"><?=$lot['description']; ?></p>
         </div>
         <div class="lot-item__right">
             <div class="lot-item__state">
@@ -53,4 +45,7 @@
             </div>
         </div>
     </div>
+    <?php else: ?>
+        <h1 style="color: black">Лот с этим ID не найден</h1>
+    <?php endif; ?>
 </section>
